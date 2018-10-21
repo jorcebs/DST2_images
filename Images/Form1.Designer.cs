b8ec536f6ec4d1002c1a769a1b849932331d8c1e
jorcebs <jor.cebs@gmail.com>
2018-10-20 19:56:11 -0500

    Upload

---
+++ Images/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Images
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTexto = new System.Windows.Forms.Button();
+            this.sfd1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTexto
+            // 
+            this.btnTexto.AutoSize = true;
+            this.btnTexto.Location = new System.Drawing.Point(44, 14);
+            this.btnTexto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTexto.Name = "btnTexto";
+            this.btnTexto.Size = new System.Drawing.Size(132, 35);
+            this.btnTexto.TabIndex = 0;
+            this.btnTexto.Text = "Archivo de texto";
+            this.btnTexto.UseVisualStyleBackColor = true;
+            this.btnTexto.Click += new System.EventHandler(this.btnTexto_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(49, 59);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Archivo binario";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnBinario_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 105);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnTexto);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Imágenes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTexto;
+        private System.Windows.Forms.SaveFileDialog sfd1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Images/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Images
+{
+    public partial class Form1 : Form
+    {
+        int i = 32565;
+        Int16 i16 = 65;
+        Int64 i64 = 65;
+        double d = 3.141592;
+        string s = "Esta es una cadena";
+        byte b = 64;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTexto_Click(object sender, EventArgs e)
+        {
+            sfd1.ShowDialog();
+            FileStream fs = new FileStream(sfd1.FileName, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(i);
+            sw.WriteLine(i16);
+            sw.WriteLine(i64);
+            sw.WriteLine(d);
+            sw.WriteLine(s);
+            sw.WriteLine(b);
+            sw.Close();
+            fs.Close();
+        }
+
+        private void btnBinario_Click(object sender, EventArgs e)
+        {
+            sfd1.ShowDialog();
+            FileStream fs = new FileStream(sfd1.FileName, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(i);
+            bw.Write(i16);
+            bw.Write(i64);
+            bw.Write(d);
+            bw.Write(s);
+            bw.Write(b);
+            bw.Close();
+            fs.Close();
+        }
+    }
+}
